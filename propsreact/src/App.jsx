2e97c3da9cdf927854585b11129d5a57d139a7bc
Harshitnakrani <nakraniharshit629@gmail.com>
2025-01-08 14:43:55 +0000

    harshit

--- propsreact/src/App.jsx
+++ propsreact/src/App.jsx
@@ -6,8 +6,8 @@ function App(props) {
     
   return (
     <>
-    
-    
+
+
     <h1 className='bg-green-400 mb-5 w-50 rounded-xl  text-center p-3'>CARD WITH PROPS</h1>
     <Card nam = "harshit"/>
     <Card nam = "nirav"/>
